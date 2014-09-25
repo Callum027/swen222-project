6c61c571beb86553ca04cc32a3b699266ba371a1
Harry <ha_zm_an@hotmail.com>
2014-09-25 14:55:03 +1200

    Update Main.java
    Gets rid of the fullscreen stuff and now doesn't require a command line argument to run :)

--- Main.java
+++ Main.java
@@ -1,24 +1,18 @@
+package game;
+
+import game.ui.*;
 
 public class Main {
 
 	private StartWindow startWindow;
 	public static GameWindow gameWindow;
-	
-	public Main(String arr){
-		if(arr.equals("fullscreen")){
-			startWindow = new StartWindow(true);
-		}
-		else if(arr.equals("windowed")){
-			startWindow = new StartWindow(false);
-		}
+
+	public Main() {
+
+		startWindow = new StartWindow();
 	}
-	
-	public static void main(String arr[]){
-		if(arr[0].equals("fullscreen")){
-			new Main(arr[0]);
-		}
-		else{
-			new Main("windowed");
-		}
+
+	public static void main(String arr[]) {
+		new Main();
 	}
 }
