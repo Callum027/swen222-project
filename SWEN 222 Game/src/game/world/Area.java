90d037fddbdc5ff2465d56cee73643d061da3c3a
Nick <NTranVUW@gmail.com>
2014-09-28 17:11:05 +1300

    Initialized the Area class. Added relationships and a constructor based on the class diagram. JavaDoc comments and comments are also provided.

--- SWEN 222 Game/src/game/world/Area.java
+++ SWEN 222 Game/src/game/world/Area.java
@@ -1,5 +1,26 @@
 package game.world;
 
+import game.world.items.Item;
+import game.world.tiles.Tile;
+
+/**
+ * An area of the Game world 
+ * @author Nick Tran
+ *
+ */
 public class Area {
+	
+	private GameWorld world;
+	private Tile[][] tiles; //the tiles that make up this area
+	private Item[] items; //the items located on this area
+	
+	/**
+	 * The constructor for the area
+	 * It takes a GameWorld object which is the GameWorld in which this area is contained in
+	 * @param world the world that contains this area
+	 */
+	public Area(GameWorld world){
+		this.world = world;
+	}
 
 }
