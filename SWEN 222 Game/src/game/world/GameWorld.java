35753de772316f3fa4e0b90ac5cd48045a75210d
Nick <NTranVUW@gmail.com>
2014-09-28 20:42:05 +1300

    Initalized the GameWorld class. Not a lot done here besides implementing a method for adding areas and a getter for the areas.

--- SWEN 222 Game/src/game/world/GameWorld.java
+++ SWEN 222 Game/src/game/world/GameWorld.java
@@ -1,7 +1,35 @@
 package game.world;
 
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * A class to represent the Game World and stores the areas that make up this "world"
+ * @author Nick Tran
+ *
+ */
 public class GameWorld {
 	
+	private ArrayList<Area> areas;
 	
+	/**
+	 * adds areas to the world
+	 * @param file the file that stores the grid of tiles
+	 * @return true is the add was successful
+	 */
+	public boolean addArea(File file){
+		if (areas.add(new Area(this, file))){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Retrieves the areas that make up the game world
+	 * @return the ArrayList of areas
+	 */
+	public ArrayList<Area> getAreas(){
+		return areas;
+	}
 
 }
