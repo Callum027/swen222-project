68e87e1516f58378dc10ba1a6ec8d1f99c878faa
sheriddavi <sheriddavi@saffron.ecs.vuw.ac.nz>
2014-10-01 12:59:09 +1300

    RenderingPanel
    Started the RenderingPanel class. Has been set up so that it can take an
    Area to render, as well as overriding the paintComponent method so that
    it can draw that Area from one direction. I've also made small changes
    to the Area and Main classes, so that we can temporary import data from
    standard text files.

--- SWEN 222 Game/src/game/Main.java
+++ SWEN 222 Game/src/game/Main.java
@@ -1,16 +1,31 @@
 package game;
 
 import java.awt.Cursor;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import game.ui.*;
 
 public class Main {
 
 	private GameFrame gameWindow;
+	private static final String IMAGE_PATH = "ui/graphics/";
 
 	public Main() {
-
+		File tiles = new File("tiles.txt");
 		gameWindow = new GameFrame(1280, 720, Cursor.getDefaultCursor());
+	}
+	
+
+	public static Image getImage(String filename){
+		java.net.URL imageURL = Main.class.getResource(IMAGE_PATH+filename);
+		try{
+			Image image = ImageIO.read(imageURL);
+			return image;
+		}catch(IOException e){throw new RuntimeException("Unable to locate "+filename);}
 	}
 
 	public static void main(String arr[]) {
