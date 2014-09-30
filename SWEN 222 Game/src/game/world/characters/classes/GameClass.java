00c086f71f1c46ec4b078f0df26522e5700bf0e4
Nick Tran <trannich@bistro-breton.ecs.vuw.ac.nz>
2014-10-01 11:07:29 +1300

    Added JavaDoc comments to the GameClass interface.

--- SWEN 222 Game/src/game/world/characters/classes/GameClass.java
+++ SWEN 222 Game/src/game/world/characters/classes/GameClass.java
@@ -1,11 +1,28 @@
 package game.world.characters.classes;
 
+/**
+ * The strategy interface for the game classes
+ * @author Nick Tran
+ *
+ */
 public interface GameClass {
 
+	/**
+	 * Retrieves the classes base strength
+	 * @return the strength of the class
+	 */
 	public int getStrength();
 
+	/**
+	 * Retrieves the classes base dexterity
+	 * @return the dexterity of the class
+	 */
 	public int getDexterity();
 
+	/**
+	 * Retrieves the classes base intelligence
+	 * @return the intelligence of the class
+	 */
 	public int getIntelligence();
 
 }
