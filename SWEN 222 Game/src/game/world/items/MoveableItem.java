7e622f4cb9b337f2f67885b800c0578a677414a4
Nick Tran <trannich@bistro-breton.ecs.vuw.ac.nz>
2014-10-01 16:46:31 +1300

    Added JavaDoc comments to moveableItem.

--- SWEN 222 Game/src/game/world/items/MoveableItem.java
+++ SWEN 222 Game/src/game/world/items/MoveableItem.java
@@ -1,7 +1,19 @@
 package game.world.items;
 
+/**
+ * Used to differentiate between items that you can move and pick-up from items that you can't
+ * @author Nick Tran
+ *
+ */
 public class MoveableItem extends Item{
 
+	/**
+	 * The Constructor
+	 * @param x the x coordiante
+	 * @param y the y coordiante
+	 * @param height the height (size) of the item
+	 * @param name the anme of the item
+	 */
 	public MoveableItem(int x, int y, int height, String name) {
 		super(x, y, height, name);
 	}
