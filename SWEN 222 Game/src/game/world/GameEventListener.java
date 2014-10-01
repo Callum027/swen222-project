9cab92e461c45fd35f7659b106febde2959e39be
Callum Dickinson <callum@huttradio.co.nz>
2014-10-01 15:30:19 +1300

    Javadoc comments for the listener interfaces

--- SWEN 222 Game/src/game/world/GameEventListener.java
+++ SWEN 222 Game/src/game/world/GameEventListener.java
@@ -3,5 +3,10 @@ package game.world;
 import java.util.EventListener;
 
 public interface GameEventListener extends EventListener {
+	/**
+	 * Invoked when a game event occurs.
+	 * 
+	 * @param ge GameEvent representing occured event
+	 */
 	public void gameEventOccurred(GameEvent ge);
 }
--- SWEN 222 Game/src/game/world/GameStateListener.java
+++ SWEN 222 Game/src/game/world/GameStateListener.java
@@ -3,5 +3,10 @@ package game.world;
 import java.util.EventListener;
 
 public interface GameStateListener extends EventListener {
+	/**
+	 * Invoked when a change in game state occurs.
+	 * 
+	 * @param gs GameState object with changes
+	 */
 	public void gameStateChanged(GameState gs);
 }
