c362660963cf7c15fc8066b114eb5f60e7154233
ToxicTsundere <callenvuw@gmail.com>
2014-10-01 21:51:50 +1300

    Updated basic parser
    -Removed packages that were not needed, updated comments in parser,
    basically so far it is creating a game world, not sure if that is needed
    or not at this point in time so am leaving in.

--- SWEN 222 Game/src/game/loading/Parser.java
+++ SWEN 222 Game/src/game/loading/Parser.java
@@ -5,18 +5,19 @@ import game.world.GameWorld;
 import java.io.File;
 import java.util.Scanner;
 /**
- * This Class holds all the methods for loading a world from an xml file.(will make comments look swanky later.
+ * This Class holds all the methods for loading a world from an xml file (will make comments look swanky later).
  * @author Tsun
  *
  */
 public class Parser {
+	//probably obsolete but will leave in for now in case i need it later
 public GameWorld parseWorld(Scanner scan){
 	try{
 	scan.useDelimiter("\\s*(?=<)|(?<=>)\\s*");
 	if(!scan.hasNext()) throw new ParserError("Empty xml file.");
 	//shouldn't get here if there is no next.
 	GameWorld world = new GameWorld();
-	if(!scan.next().equals("World"))throw new ParserError("No World declaration.");
+	if(!scan.next().equals("World"))throw new ParserError("Invalid declaration, should be a World.");
 	while(scan.hasNext()){
 		String temp = scan.next();
 		if(temp.equals("World")) break;
@@ -30,8 +31,11 @@ public GameWorld parseWorld(Scanner scan){
 }
 
 private File parseArea(Scanner scan) {
+	//am doubling up on checks atm, will fix later if i remove parseWorld.
 	try{
 		if(!scan.next().equals("Area"))throw new ParserError("Invalid declaration, should be an Area.");
+		if(!scan.hasNext()) throw new ParserError("No File declaration");
+		if(!scan.next().equals("File"))throw new ParserError("Invalid declaration, should be an Area.");
 		
 	}catch(ParserError pError){
 		System.out.println("Parser Error: "+pError.getMessage());
