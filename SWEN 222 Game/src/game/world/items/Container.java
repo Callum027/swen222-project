ed0717f7bddf49bb56119ff0af1a52c41e464424
Nick Tran <trannich@bistro-breton.ecs.vuw.ac.nz>
2014-10-01 14:22:56 +1300

    Gave the piece of furniture an interact method.

--- SWEN 222 Game/src/game/world/items/Container.java
+++ SWEN 222 Game/src/game/world/items/Container.java
@@ -9,4 +9,20 @@ public class Container extends Item{
 		super(x, y, height, name);
 	}
 
+	public int getCats() {
+		return cats;
+	}
+
+	public void setCats(int cats) {
+		this.cats = cats;
+	}
+
+	public Equipment[] getLoot() {
+		return loot;
+	}
+
+	public void setLoot(Equipment[] loot) {
+		this.loot = loot;
+	}
+
 }
--- SWEN 222 Game/src/game/world/items/Equipment.java
+++ SWEN 222 Game/src/game/world/items/Equipment.java
@@ -6,8 +6,11 @@ public class Equipment extends MoveableItem{
 	private int attack;
 	private int defence;
 
-	public Equipment(int x, int y, int height, String name) {
+	public Equipment(int x, int y, int height, String name, int attack, int defence, int worth) {
 		super(x, y, height, name);
+		this.attack = attack;
+		this.defence = defence;
+		this.worth = worth;
 	}
 
 	public int getWorth() {
@@ -16,6 +19,22 @@ public class Equipment extends MoveableItem{
 
 	public void setWorth(int worth) {
 		this.worth = worth;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getDefence() {
+		return defence;
+	}
+
+	public void setDefence(int defence) {
+		this.defence = defence;
 	}
 
 }
