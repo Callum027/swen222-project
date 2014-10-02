8a6004b0a86c88e1e560e9bd8e8e6fb923481523
Callum Dickinson <callum@huttradio.co.nz>
2014-10-02 14:13:38 +1300

    New NetIO class with static methods to help transfer primitive types over the network.

---
+++ SWEN 222 Game/src/game/net/NetIO.java
@@ -0,0 +1,152 @@
+package game.net;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.omg.CORBA_2_3.portable.InputStream;
+
+
+public class NetIO {
+	/** Size of a byte on network input/output. */
+	public static final int BYTE_LENGTH = 1;
+	/** Size of a short on network input/output. */
+	public static final int SHORT_LENGTH = 2;
+	/** Size of an int on network input/output. */
+	public static final int INTEGER_LENGTH = 4;
+	/** Size of a long on network input/output. */
+	public static final int LONG_LENGTH = 8;
+	
+	/**
+	 * Read a byte from the input stream.
+	 * @param is Input stream
+	 * @return Byte
+	 * @throws IOException
+	 */
+	public static byte readByte(InputStream is) throws IOException {
+		return (byte)is.read();
+	}
+	
+	/**
+	 * Read a short from the input stream.
+	 * @param is Input stream
+	 * @return Short
+	 * @throws IOException
+	 */
+	public static short readShort(InputStream is) throws IOException {
+		short s;
+		byte[] bytes = new byte[SHORT_LENGTH];
+		is.read(bytes);
+		
+		s = (short)((bytes[0] << 8) | bytes[1]);
+		return s;
+	}
+	
+	/**
+	 * Read an int from the input stream.
+	 * @param is Input stream
+	 * @return Integer
+	 * @throws IOException
+	 */
+	public static int readInt(InputStream is) throws IOException {
+		int i;
+		byte[] bytes = new byte[INTEGER_LENGTH];
+		is.read(bytes);
+		
+		i = (int)((bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]);
+		return i;
+	}
+	
+	/**
+	 * Read a long from the input stream.
+	 * @param is Input stream
+	 * @return Long
+	 * @throws IOException
+	 */
+	public static long readLong(InputStream is) throws IOException {
+		long l;
+		byte[] bytes = new byte[LONG_LENGTH];
+		is.read(bytes);
+		
+		l = (int)((bytes[0] << 56)
+				| (bytes[1] << 48)
+				| (bytes[2] << 40)
+				| (bytes[3] << 32)
+				| (bytes[4] << 24)
+				| (bytes[5] << 16)
+				| (bytes[6] << 8)
+				| bytes[7]);
+		
+		return l;
+	}
+	
+	/**
+	 * Write a byte to the output stream.
+	 * 
+	 * @param os Output stream
+	 * @param b Byte
+	 * @throws IOException
+	 */
+	public static void writeByte(OutputStream os, byte b) throws IOException
+	{
+		os.write((int)b);
+	}
+	
+	/**
+	 * Write a short to the output stream.
+	 * 
+	 * @param os Output stream
+	 * @param s Short
+	 * @throws IOException
+	 */
+	public static void writeShort(OutputStream os, short s) throws IOException
+	{
+		byte[] bytes = new byte[SHORT_LENGTH];
+		
+		bytes[0] = (byte)(s >> 8);
+		bytes[1] = (byte)s;
+		
+		os.write(bytes);
+	}
+	
+	/**
+	 * Write an int to the output stream.
+	 * 
+	 * @param os Output stream
+	 * @param i Integer
+	 * @throws IOException
+	 */
+	public static void writeInt(OutputStream os, int i) throws IOException
+	{
+		byte[] bytes = new byte[INTEGER_LENGTH];
+		
+		bytes[0] = (byte)((i & 0xFF000000) >> 24);
+		bytes[1] = (byte)((i & 0x00FF0000) >> 16);
+		bytes[2] = (byte)((i & 0x0000FF00) >> 8);
+		bytes[3] = (byte)i;
+		
+		os.write(bytes);
+	}
+	
+	/**
+	 * Write a long to the output stream.
+	 * 
+	 * @param os Output stream
+	 * @param l Long
+	 * @throws IOException
+	 */
+	public static void writeLong(OutputStream os, long l) throws IOException
+	{
+		byte[] bytes = new byte[LONG_LENGTH];
+		
+		bytes[0] = (byte)((l & 0xFF00000000000000L) >> 56);
+		bytes[1] = (byte)((l & 0x00FF000000000000L) >> 48);
+		bytes[2] = (byte)((l & 0x0000FF0000000000L) >> 40);
+		bytes[3] = (byte)((l & 0x000000FF00000000L) >> 32);
+		bytes[4] = (byte)((l & 0x00000000FF000000L) >> 24);
+		bytes[5] = (byte)((l & 0x0000000000FF0000L) >> 16);
+		bytes[6] = (byte)((l & 0x000000000000FF00L) >> 8);
+		bytes[7] = (byte)l;
+		
+		os.write(bytes);
+	}
+}
