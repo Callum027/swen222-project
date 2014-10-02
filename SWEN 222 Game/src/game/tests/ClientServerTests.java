e4bb6e6c628e31897d37a12203a77dee9a58dddc
Callum Dickinson <callum@huttradio.co.nz>
2014-10-02 15:51:19 +1300

    Rudimentary client-server test. Still needs work

--- SWEN 222 Game/src/game/tests/ClientServerTests.java
+++ SWEN 222 Game/src/game/tests/ClientServerTests.java
@@ -1,5 +1,9 @@
 package game.tests;
 
+import game.control.Client;
+import game.control.Server;
+import game.world.events.MoveEvent;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,44 +14,15 @@ import org.junit.runners.Suite.SuiteClasses;
 public class ClientServerTests {
 	@Test
 	public void BasicConnectionTest() {
-		/*public static void main(String[] args)
-		{
-			String host = (args.length > 1) ? args[1] : null;
-			int port  = (args.length > 2) ? Integer.parseInt(args[2]) : Server.DEFAULT_PORT;
-
-			// Make a connection to the server.
-			Client c = new Client(host, port);
-
-			System.out.println("client: calling gameEventOccurred");
-
-			// Manual call to gameEventOccurred!
-			c.gameEventOccurred(new InteractEvent());
-
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			// Close the connection.
-			c.close();
-		}*/
-
-		/*
-			public static void main(String[] args)
-			{
-				int port  = (args.length > 1) ? Integer.parseInt(args[1]) : Server.DEFAULT_PORT;
-
-				// Make a connection to the server.
-				Server s = new Server(null);
-
-				if (s.setPort(DEFAULT_PORT))
-				{
-					s.start();
-					System.out.println("server: listening on port " + port);
-				}
-			}
-		 */
+		Client client = new Client();
+		Server server = new Server();
+		
+		server.start();
+		client.start();
+		
+		client.gameEventOccurred(new MoveEvent(0, 0, null));
+		
+		server.close();
+		client.close();
 	}
 }
