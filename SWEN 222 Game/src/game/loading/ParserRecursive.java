4b0818de7b374e5cde6956025e37f728328649b5
toxicTsundere <allenchri4@ground-up.ecs.vuw.ac.nz>
2014-10-06 16:10:10 +1300

    Recursive Parser, pls work

---
+++ SWEN 222 Game/src/game/loading/ParserRecursive.java
@@ -0,0 +1,44 @@
+package game.loading;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import game.world.Area;
+import game.world.tiles.Tile;
+
+public class ParserRecursive {
+private static String next;
+	public static Area parse(String fileName, Map<Integer, Tile> tileMap) {
+		if(fileName != null){
+		try {
+			Scanner scan = new Scanner(new File(fileName));
+			gobble(scan, "<Area>");
+			return parseArea(scan, tileMap);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ParserError e) {
+			System.out.println("Parser Error: "+e.getMessage());
+		}
+		}
+		return null;
+	}
+
+	private static Area parseArea(Scanner scan, Map<Integer, Tile> tileMap) {
+
+		return null;
+	}
+
+	private static boolean gobble(Scanner scan, String s) throws ParserError {
+		if(!scan.hasNext()){
+			error("Missing Declaration");
+		}
+		return scan.next().equals(s);
+
+	}
+
+	private static void error(String message) throws ParserError {
+		throw new ParserError(message);
+	}
+}
