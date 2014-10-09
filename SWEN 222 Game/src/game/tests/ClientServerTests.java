fa802b5dcd485cf09ce0fcfb29d942a05bd05c11
Callum Dickinson <callum@huttradio.co.nz>
2014-10-09 17:53:34 +1300

    Slightly better ClientServerTests

--- SWEN 222 Game/src/game/tests/ClientServerTests.java
+++ SWEN 222 Game/src/game/tests/ClientServerTests.java
@@ -4,11 +4,15 @@ import java.awt.Point;
 
 import game.control.Client;
 import game.control.Server;
+import game.world.Area;
+import game.world.GameWorld;
 import game.world.Position;
 import game.world.characters.Player;
 import game.world.characters.classes.GameClass;
 import game.world.characters.classes.GameClass.playerClass;
 import game.world.events.MoveEvent;
+import game.world.tiles.FloorTile;
+import game.world.tiles.Tile;
 
 import org.junit.Test;
 
@@ -17,6 +21,7 @@ public class ClientServerTests {
 	private MoveEvent getTestMoveEvent() {
 		return new MoveEvent(new Position(1, 1), new Player(new Position(0, 0), "test", 0, GameClass.playerClass.ROGUE));
 	}
+
 	/*@Test
 	public void singleProcessTest() {
 		Client client = new Client();
