ae847d033aab09edc2ac1ef56ddd31ebf7ee7316
Callum Dickinson <callum@huttradio.co.nz>
2014-10-10 12:34:53 +1300

    Get rid of duplicate InvalidGameEventException

--- SWEN 222 Game/src/InvalidGameEventException.java
+++
@@ -1,9 +0,0 @@
-import game.exceptions.GameException;
-
-
-public class InvalidGameEventException extends GameException {
-	public InvalidGameEventException(byte b)
-	{
-		super("invalid game event type: " + b);
-	}
-}
