4fbfd4255f1d780161eb445ce631962bf3916a20
Nick <NTranVUW@gmail.com>
2014-10-11 03:11:47 +1300

    Added JavaDoc to Mewrcahnt

--- SWEN 222 Game/src/game/world/characters/Merchant.java
+++ SWEN 222 Game/src/game/world/characters/Merchant.java
@@ -10,10 +10,21 @@ import game.world.items.MoveableItem;
  */
 public class Merchant extends GameCharacter{
 
+	/**
+	 * The Constructor:
+	 * @param position the position that the Merchant will be spawned at
+	 * @param name the name of the merchant
+	 * @param id the unique identifier assigned to this particular character
+	 */
 	public Merchant(Position position, String name, int id){
 		super(position, name, id);
 	}
 
+	/**
+	 * the method called when the player buys an item form the merchant
+	 * @param item the item  the merchant is selling to the player
+	 * @param player the player the merchant is selling to
+	 */
 	public void sellWares(MoveableItem item, Player player){
 		player.setCats(player.getCats()-item.getWorth()); //deducts money from the player
 		player.getItems().add(item);
