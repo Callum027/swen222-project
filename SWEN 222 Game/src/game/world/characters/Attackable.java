ca5098c1933a23bcaacebfb888f57f811dc1a650
Nick Tran <trannich@bistro-breton.ecs.vuw.ac.nz>
2014-10-10 16:16:29 +1300

    added JavaDoc comments to the ATtackable interface

--- SWEN 222 Game/src/game/world/characters/Attackable.java
+++ SWEN 222 Game/src/game/world/characters/Attackable.java
@@ -1,9 +1,21 @@
 package game.world.characters;
 
+/**
+ * an interface that defines a character as an attackable object
+ * defines a character that can attack and can be attacked
+ * @author Nick Tran
+ *
+ */
 public interface Attackable {
 
+	/**
+	 * handles the logic when a character attacks a target
+	 */
 	public void attack();
 
+	/**
+	 * calculates the damage the character deals to another character
+	 */
 	public void calculateDamage();
 
 }
