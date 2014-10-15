198c8a7700e153fde3c61175ebfd9c908a675faa
toxicTsundere <allenchri4@ground-up.ecs.vuw.ac.nz>
2014-10-15 16:48:14 +1300

    Refactored Characters1.xml to Characters.xml

---
+++ SWEN 222 Game/src/game/tests/ParserTests.java
@@ -0,0 +1,17 @@
+package game.tests;
+
+import org.junit.Test;
+
+/**
+ * Tests to see if Areas are being created properly.
+ * 
+ * @author Chris Allen
+ * 
+ */
+public class ParserTests {
+
+	@Test
+	public void AreaParserTest0() {
+		
+	}
+}
